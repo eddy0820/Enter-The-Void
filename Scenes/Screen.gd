48fa1353dce88e085d14e2df296ff40b1bbdb7f8
butterysticky <88571966+butterysticky@users.noreply.github.com>
2022-04-29 16:42:26 -0400

    234234234

---
+++ Scenes/Screen.gd
@@ -0,0 +1,15 @@
+extends MeshInstance
+
+
+func _ready():
+
+  yield(get_tree(), "idle_frame")
+  yield(get_tree(), "idle_frame")
+
+  var gui_img = $GUI.get_texture()
+
+  var material = SpatialMaterial.new()
+  material.flags_transparent = true
+  material.flags_unshaded = true
+  material.albedo_texture = gui_img
+  set_surface_material(0, material)
