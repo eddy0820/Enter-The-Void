67a126894beff3ce3c31260fab2c5d2ca3cd3c4e
eddy0820 <eddye0820@gmail.com>
2022-04-29 21:48:24 -0400

    Nav Screen

---
+++ Scripts/NavigationScreenConsole.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+var onTrack = true
+var currDegree = 0
+
+
+func _process(delta):
+	if(currDegree != 0):
+		onTrack = false
+	else:
+		onTrack	= true
+		
+	if(onTrack):
+		finishNavEvent()
+
+func setNavEvent(degree: int, left: bool):
+	var string = ""
+	if(left):
+		currDegree = -degree
+		string = "left"
+	else:
+		currDegree = degree
+		string = "right"
+	$Control/NavigationContainer/SubtitleContainer/Label.text = "OBSTRUCTION DETECTED\n" + "Navigate " + str(degree) + " degrees " + string
+	
+func finishNavEvent():
+	$Control/NavigationContainer/SubtitleContainer/Label.text = ""
+	pass
+
+func rotateRight(degree: int):
+	#rotate ship here
+	currDegree += degree
+	
+	
+func rotateLeft(degree: int):
+	#rotate ship here
+	currDegree -= degree
